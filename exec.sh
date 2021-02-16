9eae4b2bb8ec4b04e7eaf98d2d7e700548d6be48
Afaq <63483862+AfaqNabi@users.noreply.github.com>
2021-02-15 19:32:21 -0700

    updates

--- exec.sh
+++ exec.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-pip install -r requirements.txt
-python3 DataGetter.py path/to/chrome or firefox/driver Chrome or firefox/version
+python3 DataGetter.py path/to/chrome_or_firefox/driver Chrome/version
--- temp.py
+++ temp.py
@@ -1,12 +1,19 @@
 import keyring
+import getpass 
+  
 
 def main():
+	email = input("Email: ")
+	try: 
+		pwd = getpass.getpass("password: ") # protected input
+	except Exception as error: 
+		print('ERROR', error) 
 	service = "login"
 	info = "info"
-	keyring.set_password(service, "username", "password") # type real username and pass
-	keyring.set_password(service, info, "username") # type real username
+	keyring.set_password(service, email, pwd) # type real username and pass
+	keyring.set_password(service, info, email) # type real username
 
-	
+
 	### getter functions to get username and pass in DataGetter.py file ###
 
 	# username = keyring.get_password(service, info)
